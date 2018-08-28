b085405a5cbc7db7180f7057d30a73518241e42e
spatten <scott@scottpatten.ca>
2018-08-27 22:16:37 -0700

    First add

---
+++ custom.el
@@ -0,0 +1,5 @@
+;; Always able to answer 'y' instead of 'yes'
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(require 'evil)
+(evil-mode 1)
---
+++ init.el
@@ -0,0 +1,38 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+; (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+; (package-initialize)
+
+; (defvar my-packages '(paredit color-theme-solarized ido-ubiquitous smex textmate magit pcre2el rust-mode lorem-ipsum haml-mode ruby-mode flymake-ruby ruby-electric web-mode yaml-mode move-text writeroom-mode ruby-hash-syntax dash rubocop exec-path-from-shell))
+(defvar my-packages '(evil))
+
+;; TODO
+
+;; To Try
+
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-hook 'after-init-hook (lambda () (load "~/.emacs.d/custom.el")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
