bcfdd85e558ca2e210523f8a888cbea3d796d570
spatten <scott@scottpatten.ca>
2018-09-16 21:20:08 -0700

    Better keys for fanfic.el

--- fanfic.el
+++ fanfic.el
@@ -2,11 +2,11 @@
   (interactive)
   (shell-command (format "%s %s" (expand-file-name "~/Dropbox/hermione/conv") (shell-quote-argument (buffer-file-name)))))
 
-(add-hook 'darkroom-mode-hook (lambda () (local-set-key (kbd "C-c c") 'convert-to-fanfic)))
+(add-hook 'darkroom-mode-hook (lambda () (local-set-key (kbd "C-c f c") 'convert-to-fanfic)))
 
 (defun open-fanfic-html ()
   (interactive)
   (shell-command (format "open output/%s.html" (shell-quote-argument (file-name-base)))))
 
 
-(add-hook 'darkroom-mode-hook (lambda () (local-set-key (kbd "C-c o") 'open-fanfic-html)))
+(add-hook 'darkroom-mode-hook (lambda () (local-set-key (kbd "C-c f o") 'open-fanfic-html)))
