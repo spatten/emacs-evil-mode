0e3bbe7beb951d6ecf61f5aac5d637e13f8c05bb
spatten <scott@scottpatten.ca>
2018-10-18 22:12:39 -0700

    Update for js editing and flycheck

--- custom.el
+++ custom.el
@@ -67,11 +67,17 @@
 (add-hook 'ruby-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
 ;; For Javascript
 (add-hook 'js2-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
+(add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
+
+;; Turn off js2's syntax checking as we're using flycheck instead
+(setq js2-mode-show-parse-errors nil)
+(setq js2-mode-show-strict-warnings nil)
 
 (require 'haml-mode)
 
 ;; JS
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
 
 ;; Coffeescript
 (require 'coffee-mode)
@@ -155,11 +161,25 @@
 
 (global-set-key (kbd "M-=") 'count-words)
 
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 ;; web-mode setup
 ;; (require 'web-mode)
 ;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 ;; https://stackoverflow.com/questions/3669511/the-function-to-show-current-files-full-path-in-mini-buffer
 (defun show-file-name ()
