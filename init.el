03bfe3b199d2df87c1db2ff72446dc7031a04a99
spatten <scott@scottpatten.ca>
2019-12-17 15:07:29 -0800

    Make TODO in org-mode less eye-straining

--- init.el
+++ init.el
@@ -48,4 +48,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-todo ((t (:background nil :foreground "red" :weight bold)))))
