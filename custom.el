d3edbde8c3cf97c0d97d7b6b46f9a65ce07df798
spatten <scott@fossa.com>
2020-01-20 20:05:19 -0800

    Better tide setup

--- custom.el
+++ custom.el
@@ -145,20 +145,26 @@
   (company-mode +1))
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-(use-package tide
-  :ensure t
-  :after (typescript-mode)
-  :config
-  (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
-  ;; (flycheck-add-next-checker 'jsx-tide '(warning . javascript-eslint) 'append)
-  ;; (flycheck-add-next-checker 'tsx-tide '(warning . javascript-eslint) 'append)
-  :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         ))
+;; (add-hook 'typescript-mode-hook #'setup-tide-mode)
+;; (use-package tide
+;;   :ensure t
+;;   :after (typescript-mode)
+;;   :config
+;;   (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
+;;   ;; (flycheck-add-next-checker 'jsx-tide '(warning . javascript-eslint) 'append)
+;;   ;; (flycheck-add-next-checker 'tsx-tide '(warning . javascript-eslint) 'append)
+;;   :hook ((typescript-mode . tide-setup)
+;;          (typescript-mode . tide-hl-identifier-mode)
+;;          ))
 (use-package tide
   :ensure t
   :after (typescript-mode company flycheck)
+  :config
+  (tide-setup)
+  (company-mode +1)
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
   :hook ((typescript-mode . tide-setup)
          (typescript-mode . tide-hl-identifier-mode)))
 ;; Coffeescript
